b4a16cf77ac074bee9d9318bf1789109b557a63c
travyu <travisyu515@gmail.com>
2025-10-22 13:27:16 -0500

    E: added group and container

--- main.py
+++ main.py
@@ -9,29 +9,38 @@ def main():
 
     pygame.init()
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
-
     clock = pygame.time.Clock()
-    dt = 0
+    
 
-    player = Player(SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2)   
+    # creating groups 
+    updatable = pygame.sprite.Group()
+    drawable = pygame.sprite.Group()
+
+    # creating a player container for the groups
+    Player.containers = (updatable, drawable)
+
+    # instantiate the player after the container to group players
+    Player(SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2)
+
+    dt = 0
 
     while True:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return
             
-        player.update(dt)
+        updatable.update(dt)
 
         screen.fill("black")
-        player.update(dt)
-        player.draw(screen)
+
+        for sprite in drawable:
+            sprite.draw(screen)
+            
         pygame.display.flip()
 
         # limit the framerate to 60 FPS
         dt = clock.tick(60) / 1000
 
         
-         
-
 if __name__ == "__main__":
     main()
