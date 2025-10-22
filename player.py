48d6f31b7e40ee3064ddd7ff0f586dc389bfc172
travyu <travisyu515@gmail.com>
2025-10-22 08:55:12 -0500

    D: added movement

--- player.py
+++ player.py
@@ -8,7 +8,7 @@ class Player(CircleShape):
         super().__init__(x, y, PLAYER_RADIUS)
         self.rotation = 0
 
-    # in the player class
+    # Asteroid player triangle 
     def triangle(self):
         forward = pygame.Vector2(0, 1).rotate(self.rotation)
         right = pygame.Vector2(0, 1).rotate(self.rotation + 90) * self.radius / 1.5
@@ -17,16 +17,24 @@ class Player(CircleShape):
         c = self.position - forward * self.radius + right
         return [a, b, c]
     
-    # this method to rotate the player
+    # rotating the player
     def rotate(self, dt):
         self.rotation += PLAYER_TURN_SPEED * dt
 
+    # updating the player poistion when keys are pressed
     def update(self, dt):
         keys = pygame.key.get_pressed()
 
         if keys[pygame.K_a]:
-            dt *= -1
-            self.rotate(dt)
+            self.rotate(-dt)
         if keys[pygame.K_d]:
             self.rotate(dt)
+        if keys[pygame.K_w]:
+            self.moves(dt)
+        if keys[pygame.K_s]:
+            self.moves(-dt)
         
+    # move the player in the game
+    def moves(self, dt):
+        forward = pygame.Vector2(0, 1).rotate(self.rotation)
+        self.position += forward * PLAYER_SPEED * dt
