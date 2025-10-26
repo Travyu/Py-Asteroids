428a33d139f8c7c46a030745eb92c38ea47c32f2
travyu <travisyu515@gmail.com>
2025-10-26 05:53:44 -0500

    I: added shooting timer

--- player.py
+++ player.py
@@ -8,6 +8,7 @@ class Player(CircleShape):
     def __init__(self, x, y):
         super().__init__(x, y, PLAYER_RADIUS)
         self.rotation = 0
+        self.timer = 0
 
     # Asteroid player triangle 
     def triangle(self):
@@ -24,6 +25,7 @@ class Player(CircleShape):
 
     # updating the player poistion when keys are pressed
     def update(self, dt):
+        self.timer -= dt
         keys = pygame.key.get_pressed()
 
         if keys[pygame.K_a]:
@@ -44,6 +46,9 @@ class Player(CircleShape):
     
     # player to shoot bullets
     def shoot(self):
+        if self.timer > 0:
+            return
+        self.timer = PLAYER_SHOOT_COOLDOWN
         shot = Shot(self.position.x, self.position.y, SHOT_RADIUS)
         shot.velocity = pygame.Vector2(0, 1).rotate(self.rotation) * PLAYER_SHOOT_SPEED
         
